c09c0ac53a00d8f3eccf7b0d1763c1c9c084cc63
Sophie Choi <103692454+destructivebasket@users.noreply.github.com>
2025-06-13 14:47:53 -0700

    added comments

--- jellyfish-clock/src/Components/DigitalClock.jsx
+++ jellyfish-clock/src/Components/DigitalClock.jsx
@@ -4,6 +4,7 @@ function DigitalClock() {
 
     const [time, setTime] = useState(new Date());
 
+    //updates the timer every second and stops when the app closes
     useEffect(() => {
         const intervalId = setInterval(() => {
             setTime(new Date());
@@ -15,17 +16,21 @@ function DigitalClock() {
     }, []);
 
     function formatTime() {
+
+        //create variables for hours, minute, and second
         let hours = time.getHours();
         const minutes = time.getMinutes();
         const seconds = time.getSeconds();
         const meridiem = hours >= 12 ? "PM" : "AM";
 
+        //ensures that hours wont be in military time
         hours = hours % 12 || 12;
 
-
+        //displays the time
         return `${padZero(hours)}:${padZero(minutes)}:${padZero(seconds)} ${meridiem}`
     }
 
+    //shows 0s when time is in the 1 digits
     function padZero (number) {
         return (number < 10 ? "0" : "") + number;
     }

cc6e3d4a4367eb6dd3af7c521b9d9a15db6892c6
Sophie Choi <103692454+destructivebasket@users.noreply.github.com>
2025-06-16 19:56:58 -0700

    jelly works now

--- jellyfish-clock/src/App.jsx
+++ jellyfish-clock/src/App.jsx
@@ -3,13 +3,14 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 import DigitalClock from './Components/DigitalClock'
+import jellyfish from "./assets/jellyfish.GIF"
 
 function App() {
 
   return (
     <div> 
       <div id = "jellyfish">
-        <img src = './jellyfish.GIF' alt = "JellyFish"/>
+        <img src = {jellyfish} alt = "JellyFish"/>
       </div>
       <div>
         <DigitalClock/>
